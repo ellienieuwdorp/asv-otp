ellienieuwdorp/asv-otp

commit d38eba6c9ca530bfcdd3ef61788609dc6c163175

Filter out nonsensical bike-to-transit options reported by TriMet

src/main/java/org/opentripplanner/api/resource/GraphPathToTripPlanConverter.java
@@ -81,11 +81,27 @@ public abstract class GraphPathToTripPlanConverter {
 
         TripPlan plan = new TripPlan(from, to, request.getDateTime());
 
+        // Convert GraphPaths to Itineraries, keeping track of the best non-transit (e.g. walk/bike-only) option time
+        long bestNonTransitTime = Long.MAX_VALUE;
+        Set<Itinerary> itineraries = new HashSet<>();
         for (GraphPath path : paths) {
             Itinerary itinerary = generateItinerary(path, request.showIntermediateStops, request.disableAlertFiltering, requestedLocale);
             itinerary = adjustItinerary(request, itinerary);
+            if(itinerary.transitTime == 0 && itinerary.walkTime < bestNonTransitTime) {
+                bestNonTransitTime = itinerary.walkTime;
+            }
+            itineraries.add(itinerary);
+        }
+
+        // Filter and add itineraries to plan
+        for (Itinerary itinerary : itineraries) {
+            // If this is a transit option whose walk/bike time is greater than that of the walk/bike-only option,
+            // do not include in plan
+            if(itinerary.transitTime > 0 && itinerary.walkTime > bestNonTransitTime) continue;
+
             plan.addItinerary(itinerary);
         }
+
         if (plan != null) {
             for (Itinerary i : plan.itinerary) {
                 /* Communicate the fact that the only way we were able to get a response was by removing a slope limit. */

commit 7813d1492da1055146b28953223ec792b352d55d

Fix regression w/ itinerary sorting introduced in d38eba6. Should address #2398

src/main/java/org/opentripplanner/api/resource/GraphPathToTripPlanConverter.java
@@ -83,7 +83,7 @@ public abstract class GraphPathToTripPlanConverter {
 
         // Convert GraphPaths to Itineraries, keeping track of the best non-transit (e.g. walk/bike-only) option time
         long bestNonTransitTime = Long.MAX_VALUE;
-        Set<Itinerary> itineraries = new HashSet<>();
+        List<Itinerary> itineraries = new LinkedList<>();
         for (GraphPath path : paths) {
             Itinerary itinerary = generateItinerary(path, request.showIntermediateStops, request.disableAlertFiltering, requestedLocale);
             itinerary = adjustItinerary(request, itinerary);