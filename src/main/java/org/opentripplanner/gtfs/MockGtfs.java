tim-mickelson/OpenTripPlanner

Prepare Netex import by extracting 'trip pattern generation' and 'stop times repair' out of GTFSPatternHopFactory.java

src/main/java/org/opentripplanner/gtfs/MockGtfs.java
@@ -3,7 +3,7 @@ package org.opentripplanner.gtfs;
 import org.opentripplanner.model.FeedId;
 import org.opentripplanner.gtfs.mapping.AgencyAndIdMapper;
 import org.opentripplanner.gtfs.mapping.OtpTransitDaoMapper;
-import org.opentripplanner.model.OtpTransitService;
+import org.opentripplanner.model.impl.OtpTransitBuilder;
 
 import java.io.File;
 import java.io.IOException;
@@ -40,12 +40,12 @@ public class MockGtfs {
         gtfsDelegate.putLines(fileName, rows);
     }
 
-    public OtpTransitService read() throws IOException {
-        return OtpTransitDaoMapper.mapDao(gtfsDelegate.read());
+    public OtpTransitBuilder read() throws IOException {
+        return OtpTransitDaoMapper.mapGtfsDaoToBuilder(gtfsDelegate.read());
     }
 
-    public OtpTransitService read(org.onebusaway.gtfs.serialization.GtfsReader reader) throws IOException {
-        return OtpTransitDaoMapper.mapDao(gtfsDelegate.read(reader));
+    public OtpTransitBuilder read(org.onebusaway.gtfs.serialization.GtfsReader reader) throws IOException {
+        return OtpTransitDaoMapper.mapGtfsDaoToBuilder(gtfsDelegate.read(reader));
     }
 
     public void putMinimal() {

src/test/java/org/opentripplanner/model/impl/OtpTransitServiceImplTest.java
@@ -2,41 +2,33 @@ package org.opentripplanner.model.impl;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.opentripplanner.gtfs.GtfsContextBuilder;
 import org.opentripplanner.model.Agency;
 import org.opentripplanner.model.FeedId;
 import org.opentripplanner.model.FareAttribute;
 import org.opentripplanner.model.FareRule;
 import org.opentripplanner.model.FeedInfo;
-import org.opentripplanner.model.Frequency;
 import org.opentripplanner.model.Pathway;
-import org.opentripplanner.model.OtpTransitService;
-import org.opentripplanner.model.Route;
-import org.opentripplanner.model.ServiceCalendar;
-import org.opentripplanner.model.ServiceCalendarDate;
 import org.opentripplanner.model.ShapePoint;
 import org.opentripplanner.model.Stop;
 import org.opentripplanner.model.StopTime;
 import org.opentripplanner.model.Transfer;
 import org.opentripplanner.model.Trip;
-import org.opentripplanner.model.calendar.ServiceDate;
+import org.opentripplanner.model.OtpTransitService;
 import org.opentripplanner.ConstantsForTests;
-import org.opentripplanner.gtfs.mapping.OtpTransitDaoMapper;
 
-import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.toList;
 import static org.junit.Assert.assertEquals;
+import static org.opentripplanner.gtfs.GtfsContextBuilder.contextBuilder;
 
 public class OtpTransitServiceImplTest {
     private static final String FEED_ID = "Z";
-
-    private static final FeedId SERVICE_ALLDAYS_ID = new FeedId(FEED_ID, "alldays");
-
-    private static final FeedId SERVICE_WEEKDAYS_ID = new FeedId(FEED_ID, "weekdays");
 
     private static final FeedId STATION_ID = new FeedId(FEED_ID, "station");
 
@@ -47,20 +39,12 @@ public class OtpTransitServiceImplTest {
 
     @BeforeClass
     public static void setup() throws IOException {
-        org.onebusaway.gtfs.impl.GtfsRelationalDaoImpl dao = new org.onebusaway.gtfs.impl.GtfsRelationalDaoImpl();
+        GtfsContextBuilder contextBuilder = contextBuilder(FEED_ID, ConstantsForTests.FAKE_GTFS);
+        OtpTransitBuilder builder = contextBuilder.getTransitBuilder();
 
-        org.onebusaway.gtfs.serialization.GtfsReader reader = new org.onebusaway.gtfs.serialization.GtfsReader();
-
-        reader.setInputLocation(new File(ConstantsForTests.FAKE_GTFS));
-        reader.setEntityStore(dao);
-        reader.setDefaultAgencyId(FEED_ID);
-        reader.run();
-
-        OtpTransitBuilder builder = new OtpTransitBuilder(OtpTransitDaoMapper.mapDao(dao));
         agency = first(builder.getAgencies());
 
         // Supplement test data with at least one entity in all collections
-        builder.getCalendarDates().add(createAServiceCalendarDateExclution(SERVICE_WEEKDAYS_ID));
         FareRule rule = createFareRule();
         builder.getFareAttributes().add(rule.getFare());
         builder.getFareRules().add(rule);
@@ -77,24 +61,6 @@ public class OtpTransitServiceImplTest {
         assertEquals(1, agencies.size());
         assertEquals("agency", agency.getId());
         assertEquals("Fake Agency", agency.getName());
-    }
-
-    @Test
-    public void testGetAllCalendarDates() {
-        Collection<ServiceCalendarDate> calendarDates = subject.getAllCalendarDates();
-
-        assertEquals(1, calendarDates.size());
-        assertEquals(
-                "<CalendarDate serviceId=Z_weekdays date=ServiceIdDate(2017-8-31) exception=2>",
-                first(calendarDates).toString());
-    }
-
-    @Test
-    public void testGetAllCalendars() {
-        Collection<ServiceCalendar> calendars = subject.getAllCalendars();
-
-        assertEquals(2, calendars.size());
-        assertEquals("<ServiceCalendar Z_alldays [1111111]>", first(calendars).toString());
     }
 
     @Test
@@ -125,17 +91,6 @@ public class OtpTransitServiceImplTest {
     }
 
     @Test
-    public void testGetAllFrequencies() {
-        Collection<Frequency> frequencies = subject.getAllFrequencies();
-
-        assertEquals(2, frequencies.size());
-        assertEquals(
-                "<Frequency trip=agency_15.1 start=06:00:00 end=10:00:01>",
-                first(frequencies).toString()
-        );
-    }
-
-    @Test
     public void testGetAllPathways() {
         Collection<Pathway> pathways = subject.getAllPathways();
 
@@ -144,27 +99,11 @@ public class OtpTransitServiceImplTest {
     }
 
     @Test
-    public void testGetAllRoutes() {
-        Collection<Route> routes = subject.getAllRoutes();
-
-        assertEquals(18, routes.size());
-        assertEquals("<Route agency_1 1>", first(routes).toString());
-    }
-
-    @Test
     public void testGetAllTransfers() {
         Collection<Transfer> transfers = subject.getAllTransfers();
 
         assertEquals(9, transfers.size());
         assertEquals("<Transfer stop=Z_F..Z_E>", first(transfers).toString());
-    }
-
-    @Test
-    public void testGetAllShapePoints() {
-        Collection<ShapePoint> shapePoints = subject.getAllShapePoints();
-
-        assertEquals(9, shapePoints.size());
-        assertEquals("<ShapePoint Z_4 #1 (41.0,-75.0)>", first(shapePoints).toString());
     }
 
     @Test
@@ -177,7 +116,10 @@ public class OtpTransitServiceImplTest {
 
     @Test
     public void testGetAllStopTimes() {
-        Collection<StopTime> stopTimes = subject.getAllStopTimes();
+        List<StopTime> stopTimes = new ArrayList<>();
+        for (Trip trip : subject.getAllTrips()) {
+            stopTimes.addAll(subject.getStopTimesForTrip(trip));
+        }
 
         assertEquals(80, stopTimes.size());
         assertEquals("StopTime(seq=1 stop=Z_A trip=agency_1.1 times=00:00:00-00:00:00)",
@@ -199,17 +141,6 @@ public class OtpTransitServiceImplTest {
     }
 
     @Test
-    public void testGetTripAgencyIdsReferencingServiceId() {
-        List<String> agencyIds;
-
-        agencyIds = subject.getTripAgencyIdsReferencingServiceId(SERVICE_ALLDAYS_ID);
-        assertEquals("[agency]", agencyIds.toString());
-
-        agencyIds = subject.getTripAgencyIdsReferencingServiceId(SERVICE_WEEKDAYS_ID);
-        assertEquals("[agency]", agencyIds.toString());
-    }
-
-    @Test
     public void testGetStopsForStation() {
         List<Stop> stops = subject.getStopsForStation(subject.getStopForId(STATION_ID));
         assertEquals("[<Stop Z_A>]", stops.toString());
@@ -217,7 +148,7 @@ public class OtpTransitServiceImplTest {
 
     @Test
     public void testGetShapePointsForShapeId() {
-        List<ShapePoint> shapePoints = subject.getShapePointsForShapeId(new FeedId("Z", "5"));
+        Collection<ShapePoint> shapePoints = subject.getShapePointsForShapeId(new FeedId("Z", "5"));
         assertEquals("[#1 (41,-72), #2 (41,-72), #3 (40,-72), #4 (41,-73), #5 (41,-74)]",
                 shapePoints.stream().map(OtpTransitServiceImplTest::toString).collect(toList()).toString());
     }
@@ -231,32 +162,10 @@ public class OtpTransitServiceImplTest {
 
     @Test
     public void testGetAllServiceIds() {
-        List<FeedId> serviceIds = subject.getAllServiceIds();
+        Collection<FeedId> serviceIds = subject.getAllServiceIds();
 
         assertEquals(2, serviceIds.size());
         assertEquals("Z_alldays", first(serviceIds).toString());
-    }
-
-    @Test
-    public void testGetCalendarDatesForServiceId() {
-        List<ServiceCalendarDate> dates = subject.getCalendarDatesForServiceId(SERVICE_WEEKDAYS_ID);
-        assertEquals(
-                "[<CalendarDate serviceId=Z_weekdays date=ServiceIdDate(2017-8-31) exception=2>]",
-                dates.toString());
-    }
-
-    @Test
-    public void testGetCalendarForServiceId() {
-        ServiceCalendar calendar = subject.getCalendarForServiceId(SERVICE_ALLDAYS_ID);
-        assertEquals("<ServiceCalendar Z_alldays [1111111]>", calendar.toString());
-    }
-
-    private static FareAttribute createFareAttribute() {
-        FareAttribute fa = new FareAttribute();
-        fa.setId(new FeedId(agency.getId(), "FA"));
-        FareRule rule = new FareRule();
-        rule.setFare(fa);
-        return fa;
     }
 
     private static FareRule createFareRule() {
@@ -268,15 +177,6 @@ public class OtpTransitServiceImplTest {
         rule.setDestinationId("Zone C");
         rule.setFare(fa);
         return rule;
-    }
-
-
-    private static ServiceCalendarDate createAServiceCalendarDateExclution(FeedId serviceId) {
-        ServiceCalendarDate date = new ServiceCalendarDate();
-        date.setServiceId(serviceId);
-        date.setDate(new ServiceDate(2017, 8, 31));
-        date.setExceptionType(2);
-        return date;
     }
 
     private static <T> T first(Collection<? extends T> c) {

ellienieuwdorp/asv-otp

src/main/java/org/opentripplanner/gtfs/MockGtfs.java
@@ -1,9 +1,10 @@
 package org.opentripplanner.gtfs;
 
-import org.opentripplanner.model.FeedScopedId;
 import org.opentripplanner.gtfs.mapping.AgencyAndIdMapper;
 import org.opentripplanner.gtfs.mapping.GTFSToOtpTransitServiceMapper;
-import org.opentripplanner.model.OtpTransitService;
+import org.opentripplanner.model.FeedScopedId;
+
+import org.opentripplanner.model.impl.OtpTransitServiceBuilder;
 
 import java.io.File;
 import java.io.IOException;
@@ -40,12 +41,12 @@ public class MockGtfs {
         gtfsDelegate.putLines(fileName, rows);
     }
 
-    public OtpTransitService read() throws IOException {
-        return GTFSToOtpTransitServiceMapper.mapGtfsDaoToOTPTransitService(gtfsDelegate.read());
+    public OtpTransitServiceBuilder read() throws IOException {
+        return GTFSToOtpTransitServiceMapper.mapGtfsDaoToInternalTransitServiceBuilder(gtfsDelegate.read());
     }
 
-    public OtpTransitService read(org.onebusaway.gtfs.serialization.GtfsReader reader) throws IOException {
-        return GTFSToOtpTransitServiceMapper.mapGtfsDaoToOTPTransitService(gtfsDelegate.read(reader));
+    public OtpTransitServiceBuilder read(org.onebusaway.gtfs.serialization.GtfsReader reader) throws IOException {
+        return GTFSToOtpTransitServiceMapper.mapGtfsDaoToInternalTransitServiceBuilder(gtfsDelegate.read(reader));
     }
 
     public void putMinimal() {

src/test/java/org/opentripplanner/model/impl/OtpTransitServiceImplTest.java
@@ -2,43 +2,33 @@ package org.opentripplanner.model.impl;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.opentripplanner.gtfs.GtfsContextBuilder;
 import org.opentripplanner.model.Agency;
 import org.opentripplanner.model.FeedScopedId;
 import org.opentripplanner.model.FareAttribute;
 import org.opentripplanner.model.FareRule;
 import org.opentripplanner.model.FeedInfo;
-import org.opentripplanner.model.Frequency;
 import org.opentripplanner.model.Pathway;
-import org.opentripplanner.model.OtpTransitService;
-import org.opentripplanner.model.Route;
-import org.opentripplanner.model.ServiceCalendar;
-import org.opentripplanner.model.ServiceCalendarDate;
 import org.opentripplanner.model.ShapePoint;
 import org.opentripplanner.model.Stop;
 import org.opentripplanner.model.StopTime;
 import org.opentripplanner.model.Transfer;
 import org.opentripplanner.model.Trip;
-import org.opentripplanner.model.calendar.ServiceDate;
+import org.opentripplanner.model.OtpTransitService;
 import org.opentripplanner.ConstantsForTests;
-import org.opentripplanner.gtfs.mapping.GTFSToOtpTransitServiceMapper;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Comparator;
 import java.util.List;
 
 import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.toList;
 import static org.junit.Assert.assertEquals;
+import static org.opentripplanner.gtfs.GtfsContextBuilder.contextBuilder;
 
 public class OtpTransitServiceImplTest {
     private static final String FEED_ID = "Z";
-
-    private static final FeedScopedId SERVICE_ALLDAYS_ID = new FeedScopedId(FEED_ID, "alldays");
-
-    private static final FeedScopedId SERVICE_WEEKDAYS_ID = new FeedScopedId(FEED_ID, "weekdays");
 
     private static final FeedScopedId STATION_ID = new FeedScopedId(FEED_ID, "station");
 
@@ -49,20 +39,12 @@ public class OtpTransitServiceImplTest {
 
     @BeforeClass
     public static void setup() throws IOException {
-        org.onebusaway.gtfs.impl.GtfsRelationalDaoImpl dao = new org.onebusaway.gtfs.impl.GtfsRelationalDaoImpl();
+        GtfsContextBuilder contextBuilder = contextBuilder(FEED_ID, ConstantsForTests.FAKE_GTFS);
+        OtpTransitServiceBuilder builder = contextBuilder.getTransitBuilder();
 
-        org.onebusaway.gtfs.serialization.GtfsReader reader = new org.onebusaway.gtfs.serialization.GtfsReader();
-
-        reader.setInputLocation(new File(ConstantsForTests.FAKE_GTFS));
-        reader.setEntityStore(dao);
-        reader.setDefaultAgencyId(FEED_ID);
-        reader.run();
-
-        OtpTransitServiceBuilder builder = new OtpTransitServiceBuilder(GTFSToOtpTransitServiceMapper.mapGtfsDaoToOTPTransitService(dao));
         agency = first(builder.getAgencies());
 
         // Supplement test data with at least one entity in all collections
-        builder.getCalendarDates().add(createAServiceCalendarDateExclution(SERVICE_WEEKDAYS_ID));
         FareRule rule = createFareRule();
         builder.getFareAttributes().add(rule.getFare());
         builder.getFareRules().add(rule);
@@ -79,24 +61,6 @@ public class OtpTransitServiceImplTest {
         assertEquals(1, agencies.size());
         assertEquals("agency", agency.getId());
         assertEquals("Fake Agency", agency.getName());
-    }
-
-    @Test
-    public void testGetAllCalendarDates() {
-        Collection<ServiceCalendarDate> calendarDates = subject.getAllCalendarDates();
-
-        assertEquals(1, calendarDates.size());
-        assertEquals(
-                "<CalendarDate serviceId=Z_weekdays date=ServiceIdDate(2017-8-31) exception=2>",
-                first(calendarDates).toString());
-    }
-
-    @Test
-    public void testGetAllCalendars() {
-        Collection<ServiceCalendar> calendars = subject.getAllCalendars();
-
-        assertEquals(2, calendars.size());
-        assertEquals("<ServiceCalendar Z_alldays [1111111]>", first(calendars).toString());
     }
 
     @Test
@@ -127,17 +91,6 @@ public class OtpTransitServiceImplTest {
     }
 
     @Test
-    public void testGetAllFrequencies() {
-        Collection<Frequency> frequencies = subject.getAllFrequencies();
-
-        assertEquals(2, frequencies.size());
-        assertEquals(
-                "<Frequency trip=agency_15.1 start=06:00:00 end=10:00:01>",
-                first(frequencies).toString()
-        );
-    }
-
-    @Test
     public void testGetAllPathways() {
         Collection<Pathway> pathways = subject.getAllPathways();
 
@@ -146,27 +99,11 @@ public class OtpTransitServiceImplTest {
     }
 
     @Test
-    public void testGetAllRoutes() {
-        Collection<Route> routes = subject.getAllRoutes();
-
-        assertEquals(18, routes.size());
-        assertEquals("<Route agency_1 1>", first(routes).toString());
-    }
-
-    @Test
     public void testGetAllTransfers() {
         Collection<Transfer> transfers = subject.getAllTransfers();
 
         assertEquals(9, transfers.size());
         assertEquals("<Transfer stop=Z_F..Z_E>", first(transfers).toString());
-    }
-
-    @Test
-    public void testGetAllShapePoints() {
-        Collection<ShapePoint> shapePoints = subject.getAllShapePoints();
-
-        assertEquals(9, shapePoints.size());
-        assertEquals("<ShapePoint Z_4 #1 (41.0,-75.0)>", first(shapePoints).toString());
     }
 
     @Test
@@ -179,7 +116,10 @@ public class OtpTransitServiceImplTest {
 
     @Test
     public void testGetAllStopTimes() {
-        Collection<StopTime> stopTimes = subject.getAllStopTimes();
+        List<StopTime> stopTimes = new ArrayList<>();
+        for (Trip trip : subject.getAllTrips()) {
+            stopTimes.addAll(subject.getStopTimesForTrip(trip));
+        }
 
         assertEquals(80, stopTimes.size());
         assertEquals("StopTime(seq=1 stop=Z_A trip=agency_1.1 times=00:00:00-00:00:00)",
@@ -198,17 +138,6 @@ public class OtpTransitServiceImplTest {
     public void testGetStopForId() {
         Stop stop = subject.getStopForId(new FeedScopedId("Z", "P"));
         assertEquals("<Stop Z_P>", stop.toString());
-    }
-
-    @Test
-    public void testGetTripAgencyIdsReferencingServiceId() {
-        List<String> agencyIds;
-
-        agencyIds = subject.getTripAgencyIdsReferencingServiceId(SERVICE_ALLDAYS_ID);
-        assertEquals("[agency]", agencyIds.toString());
-
-        agencyIds = subject.getTripAgencyIdsReferencingServiceId(SERVICE_WEEKDAYS_ID);
-        assertEquals("[agency]", agencyIds.toString());
     }
 
     @Test
@@ -233,32 +162,10 @@ public class OtpTransitServiceImplTest {
 
     @Test
     public void testGetAllServiceIds() {
-        List<FeedScopedId> serviceIds = subject.getAllServiceIds();
+        Collection<FeedScopedId> serviceIds = subject.getAllServiceIds();
 
         assertEquals(2, serviceIds.size());
         assertEquals("Z_alldays", first(serviceIds).toString());
-    }
-
-    @Test
-    public void testGetCalendarDatesForServiceId() {
-        List<ServiceCalendarDate> dates = subject.getCalendarDatesForServiceId(SERVICE_WEEKDAYS_ID);
-        assertEquals(
-                "[<CalendarDate serviceId=Z_weekdays date=ServiceIdDate(2017-8-31) exception=2>]",
-                dates.toString());
-    }
-
-    @Test
-    public void testGetCalendarForServiceId() {
-        ServiceCalendar calendar = subject.getCalendarForServiceId(SERVICE_ALLDAYS_ID);
-        assertEquals("<ServiceCalendar Z_alldays [1111111]>", calendar.toString());
-    }
-
-    private static FareAttribute createFareAttribute() {
-        FareAttribute fa = new FareAttribute();
-        fa.setId(new FeedScopedId(agency.getId(), "FA"));
-        FareRule rule = new FareRule();
-        rule.setFare(fa);
-        return fa;
     }
 
     private static FareRule createFareRule() {
@@ -272,19 +179,9 @@ public class OtpTransitServiceImplTest {
         return rule;
     }
 
-
-    private static ServiceCalendarDate createAServiceCalendarDateExclution(FeedScopedId serviceId) {
-        ServiceCalendarDate date = new ServiceCalendarDate();
-        date.setServiceId(serviceId);
-        date.setDate(new ServiceDate(2017, 8, 31));
-        date.setExceptionType(2);
-        return date;
-    }
-
-    private static <T> T first(Collection<T> collection) {
-        List<T> items = new ArrayList<>(collection);
-        items.sort(comparing(Object::toString));
-        return items.isEmpty() ? null : items.get(0);
+    private static <T> T first(Collection<? extends T> c) {
+        //noinspection ConstantConditions
+        return c.stream().sorted(comparing(T::toString)).findFirst().get();
     }
 
     private static String toString(ShapePoint sp) {