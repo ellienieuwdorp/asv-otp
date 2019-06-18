86140b8b3675dfa17bf42b612deac94d0b7ef71a
Luuk Nieuwdorp <luuk@nieuwdorp.me>
2019-06-18 16:04:20 +0200

    finish test requirement 8

--- src/test/java/org/opentripplanner/requirements/requirement8.java
+++ src/test/java/org/opentripplanner/requirements/requirement8.java
@@ -4,10 +4,9 @@ import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.Test;
 import org.opentripplanner.standalone.config.OTPConfiguration;
 
-
 import java.io.File;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 public class requirement8 {
 
@@ -20,12 +19,15 @@ public class requirement8 {
         String urlOne = "http://gtfs.openov.nl/gtfs-rt/tripUpdates.pb";
         String urlTwo = "http://gtfs.openov.nl/gtfs-rt/trainUpdates.pb";
 
+        int iterator = 0;
         for (JsonNode configItem : configuration.path("updaters")) {
+
             String url = configItem.path("url").asText();
             if (url != null) {
-                assertTrue(url.equals(urlOne) || url.equals(urlTwo));
+                if (iterator == 0) assertTrue(url.equals(urlOne));
+                if (iterator == 1) assertTrue(url.equals(urlTwo));
             }
+            iterator++;
         }
-
     }
 }
