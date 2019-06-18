d0581f40ae276b7acea5bfbae0110873c07cae66
Luuk Nieuwdorp <luuk@nieuwdorp.me>
2019-06-18 15:36:15 +0200

    Add test for requirement 8

---
+++ src/test/java/org/opentripplanner/requirements/requirement8.java
@@ -0,0 +1,31 @@
+package org.opentripplanner.requirements;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.junit.Test;
+import org.opentripplanner.standalone.config.OTPConfiguration;
+
+
+import java.io.File;
+
+import static org.junit.Assert.*;
+
+public class requirement8 {
+
+    @Test
+    public void test() {
+        File config = new File("/Users/luuk/Development/OpenTripPlanner/src/test/java/org/opentripplanner/requirements");
+
+        OTPConfiguration otpConfiguration = new OTPConfiguration(config);
+        JsonNode configuration = otpConfiguration.getGraphConfig(config).routerConfig();
+        String urlOne = "http://gtfs.openov.nl/gtfs-rt/tripUpdates.pb";
+        String urlTwo = "http://gtfs.openov.nl/gtfs-rt/trainUpdates.pb";
+
+        for (JsonNode configItem : configuration.path("updaters")) {
+            String url = configItem.path("url").asText();
+            if (url != null) {
+                assertTrue(url.equals(urlOne) || url.equals(urlTwo));
+            }
+        }
+
+    }
+}
